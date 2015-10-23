9d3d67a08ae68cbdd347a820917f3a4c25219f73
Morane.Otilia <morane.gg@gmail.com>
2015-10-23 18:05:23 +0200

    Gestion des rôles, ajustements intersection, TODO : Correction intersection subs X

--- projet.pl
+++ projet.pl
@@ -30,15 +30,19 @@ subs(pet, some(aMaitre)).
 subs(some(aMaitre), all(aMaitre,humain)).
 subs(chihuahua,and(chien,pet)).
 
+subs(humain,mammifere).
 /*Le carnivore étant par définition un consomateur exlusif de viande, nous nous passerons du Exclusif qui dans ce contexte tient lieu de pléonasme.*/
 
 subs(carnivore,all(mange,animal)).
-subs(animal,all(mange,plante)).
+subs(all(mange,animal),carnivore).
+subs(herbivore,all(mange,plante)).
+subs(all(mange,plante),herbivore).
 
+subs(chien,carnivore).
 subs(lion,carnivore).
 subs(carnivore,predateur).
 subs(animal,some(mange)).
-subs(nothing,and(all(manger,nothing),some(mange))). %Question pour vérification.
+subs(and(all(manger,nothing),some(mange)),nothing). %Question pour vérification.
 
 /*A-box*/
 /*******************************************************************************/
@@ -51,45 +55,58 @@ subs(nothing,and(all(manger,nothing),some(mange))). %Question pour vérification
 /*Subsomption en exercice 2.1
 *ne permet pas de ____________________
 */
-/*solution trivial*/
-    subS1(C,C). 
-    /**/
-    subS1(C,D):-subs(C,D).
-    /*Par transitivite*/
-    subS1(C,D):-subs(C,Z),Z\==D,subS1(Z,D).
+	/* solutions triviales */
+		subS1(C,C). 
+		subS1(C,D):-subs(C,D).
+    /* Par transitivite */
+		subS1(C,D):-subs(C,Z),Z\==D,subS1(Z,D).
     
 /*****************************************************************************/
 /*Subsomption en exercice 2.4
 * en utilisant la fonction not_member, teste la condition que Z ne soit pas dans L
 */
-    subsS(C,C,_). 
-    subsS(C,D,_):-subs(C,D). 
-
-    subsS(C,D,L):-subs(C,Z),Z\==D,not_member(Z,L),subsS(Z,D,[Z|L]).
+	/* Permet la gestion masqué de la liste */
+		subsS(C,D):-subsS(C,D,[]).
+	/* Solutions triviales */
+		subsS(C,C,_). 
+		subsS(C,D,_):-subs(C,D). 
+	/* Transitivité */
+    	subsS(C,D,L):-subs(C,Z),Z\==D,not_member(Z,L),subsS(Z,D,[Z|L]).
 
 /*******************************************************************************/
 /*Intersection en exercice 3
-*
-*/  %Permet d'effectuer une requête de type, A subs B and C.
-    subsS(A,and(E,D),L):-subs(A,and(_,Z)),subs(A,and(X,_)),subsS(X,E,L),subsS(Z,D,L).
-    subsS(A,and(E,D),L):-subs(A,and(Z,_)),subs(A,and(_,X)),subsS(X,E,L),subsS(Z,D,L).
+*   *** Subsume indéfiniment les requètes type unification avec variable avec les parties b) et d) ***
+*/ 
+
+	 %a) Permet d'effectuer une requête de type, A subs B and C uniquement les cas contenant and, les traitements unitaires sont gérés par la partie c).
+	    subsS(A,and(E,D),L):-subs(A,and(_,Z)),subs(A,and(X,_)),subsS(E,X,L),subsS(D,Z,L).
+    	subsS(A,and(E,D),L):-subs(A,and(Z,_)),subs(A,and(_,X)),subsS(E,X,L),subsS(D,Z,L).
 
 
-    %Permet de gérer le cas particulier ou A subs B subs C -> A subs B and C (A reverifier)
-        subsS(A,and(D,_),L):-subs(A,Z),subsS(Z,D,L).
-        subsS(A,and(_,D),L):-subs(A,Z),subsS(Z,D,L).
+	%b) Devrait traiter des cas comme chihuahua,and(chien,animal) mais possède un comportement hasardeux.
+        %subsS(A,and(D,_),L):-subs(A,Z),subsS(Z,D,L).
+        %subsS(A,and(_,D),L):-subs(A,Z),subsS(Z,D,L).
         
-        %Ca je sais plus, il faut que je relise
+    %c) Permet de gérer le cas particulier ou A subs (B and C) -> (A subs B) and (A subs C) (chihuahua subs mammfere)
         subsS(A,D,L):-subs(A,and(Z,_)),subsS(Z,D,L).
         subsS(A,D,L):-subs(A,and(_,Z)),subsS(Z,D,L).
 
-    %Permet d'effectuer une requête de type, A and B subs C 
-    subsS(and(A,B),C,L):-subs(and(Z,_),C),subs(and(_,X),C),subsS(Z,A,L),subsS(X,B,L).
-    subsS(and(A,B),C,L):-subs(and(_,Z),C),subs(and(X,_),C),subsS(Z,A,L),subsS(X,B,L).
-        %Il doit manquer un traitement pour faire des trucs, je sais pas trop.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    %d) Devrait permettre d'effectuer une requête de type, A and B subs C
+		%subsS(and(A,B),C,L):-subs(and(Z,_),C),subs(and(_,X),C),subsS(Z,A,L),subsS(X,B,L).
+		%subsS(and(A,B),C,L):-subs(and(_,Z),C),subs(and(X,_),C),subsS(Z,A,L),subsS(X,B,L).
 
+		subsS(and(A,B),C,L):-subsS(Z,A),subsS(Z,B),subs(Z,C,L).
+		%subsS(and(A,B),C,L):-subs(and(_,Z),C),subs(and(X,_),C),subsS(Z,A,L),subsS(X,B,L).
 
-subsS(C,D):-subsS(C,D,[]).
+/*******************************************************************************/
+/*Gestion des rôles en exercice 4
+*   Traite les requêtes all uniquement.
+*/  %	
+
+	subsS(A,all(_,D),L):-subs(A,all(_,Z)),subsS(D,Z,L).
+
+	subsS(all(_,A),D,L):-subs(A,Z),subsS(all(_,Z),D,L).
+
 
 
 %Redondant : subsS(and(B,D),and(E,F)):-subsS(B,E), subsS(B,F),subsS(D,E), subsS(D,F). 
