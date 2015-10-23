6d09ae103d43ee675892175594a9c0378a6f216f
Morane.Otilia <morane.gg@gmail.com>
2015-10-23 20:02:58 +0200

    ajout de la A-box, commentaire sur la question 5 de complétude et une possibilité qui ne fonctionne pas pour le traitement des instances

--- projet.pl
+++ projet.pl
@@ -30,7 +30,7 @@ subs(pet, some(aMaitre)).
 subs(some(aMaitre), all(aMaitre,humain)).
 subs(chihuahua,and(chien,pet)).
 
-subs(humain,mammifere).
+
 /*Le carnivore étant par définition un consomateur exlusif de viande, nous nous passerons du Exclusif qui dans ce contexte tient lieu de pléonasme.*/
 
 subs(carnivore,all(mange,animal)).
@@ -38,14 +38,30 @@ subs(all(mange,animal),carnivore).
 subs(herbivore,all(mange,plante)).
 subs(all(mange,plante),herbivore).
 
-subs(chien,carnivore).
 subs(lion,carnivore).
 subs(carnivore,predateur).
 subs(animal,some(mange)).
 subs(and(all(manger,nothing),some(mange)),nothing). %Question pour vérification.
 
+%subs(humain,mammifere).
+%subs(chien,carnivore).
+
 /*A-box*/
 /*******************************************************************************/
+inst(felix, chat).
+inst(pierre, humain).
+inst(marie, humain).
+inst(princesse, chihuahua).
+inst(jerry, souris).
+inst(titi, canari).
+
+instR(felix, mange, jerry).
+instR(felix, mange, titi).
+instR(felix, aMaitre, pierre).
+instR(princesse, aMaitre, marie).
+
+
+
 /*trouver si un element E n'est pas memebre d'une liste L*/
     not_member(_,[]).
     not_member(E,[T|Q]):-E\==T,not_member(E,Q).
@@ -95,24 +111,35 @@ subs(and(all(manger,nothing),some(mange)),nothing). %Question pour vérification
 		%subsS(and(A,B),C,L):-subs(and(Z,_),C),subs(and(_,X),C),subsS(Z,A,L),subsS(X,B,L).
 		%subsS(and(A,B),C,L):-subs(and(_,Z),C),subs(and(X,_),C),subsS(Z,A,L),subsS(X,B,L).
 
-		subsS(and(A,B),C,L):-subsS(Z,A),subsS(Z,B),subs(Z,C,L).
+		subsS(and(A,B),C,L):-subs(A,X),subs(B,Y),subsS(and(X,Y),C).
+		%subsS(and(B,A),C,L):-subs(A,X),subs(B,Y),subsS(C,X,L),subsS(C,Y,L).
 		%subsS(and(A,B),C,L):-subs(and(_,Z),C),subs(and(X,_),C),subsS(Z,A,L),subsS(X,B,L).
 
 /*******************************************************************************/
 /*Gestion des rôles en exercice 4
 *   Traite les requêtes all uniquement.
 */  %	
+	%Gère les cas A subs VR.C -> A subs VR.D si D subs C
+		subsS(A,all(X,D),L):-subs(A,all(X,Z)),subsS(D,Z,L).
 
-	subsS(A,all(_,D),L):-subs(A,all(_,Z)),subsS(D,Z,L).
-
-	subsS(all(_,A),D,L):-subs(A,Z),subsS(all(_,Z),D,L).
+		%subsS(all(X,A),D,L):-subs(A,Z),subsS(all(X,Z),D,L).
 
 
 
 %Redondant : subsS(and(B,D),and(E,F)):-subsS(B,E), subsS(B,F),subsS(D,E), subsS(D,F). 
-/*Quantification existentielle
+
+%traitement des instances
+%inst(I,C):- subs(C,Z),Z\==C, inst(I,D).
+
+
+
+
+/*Exercice 4.5 Quantification existentielle
 some(R).*/
 
-/**/
+/*Exercice 5*/
+/* Il est complet quand toutes les déductions sémantiques peuvent se dériver dans le système. On parle de théorème de complétude quand il existe un système de déduction fidèle qui est complet (le système de déduction doit être raisonnable, c’est-à-dire que l'ensemble des preuves dans le système doit être récursif).
+par exemple le chat qui mange les animaux mais il n'est pas carnivore, car ce n'était pas défini dans notre T-box
+*/
 
 
